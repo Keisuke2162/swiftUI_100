34512b2420576868d9820d25b3025c1596ff337f
Keisuke Ueda <42376257+Keisuke2162@users.noreply.github.com>
2023-04-21 17:14:55 +0900

    プロジェクト追加 (#1)

---
+++ swiftUI_100/swiftUI_100/swiftUI_100App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_100App.swift
+//  swiftUI_100
+//
+//  Created by Keisuke Ueda on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
