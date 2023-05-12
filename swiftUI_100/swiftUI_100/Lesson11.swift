d875933b2daed81b7f51eaebb029001d4c1f8fee
Keisuke Ueda <42376257+Keisuke2162@users.noreply.github.com>
2023-05-12 17:46:28 +0900

    Lesson11

---
+++ swiftUI_100/swiftUI_100/Lesson11.swift
@@ -0,0 +1,42 @@
+//
+//  Lesson11.swift
+//  swiftUI_100
+//
+//  Created by Keisuke Ueda on 2023/05/12.
+//
+
+import SwiftUI
+
+struct Lesson11: View {
+    let fruits = ["Apple", "Banana", "Orange", "Grape", "Cherry", "Peach"]
+    var body: some View {
+        NavigationView {
+            List(fruits, id: \.self) { fruit in
+                NavigationLink(destination: Lesson11Second(fruit: fruit)) {
+                    Text(fruit)
+                }
+            }
+            Text("test")
+        }
+    }
+}
+
+struct Lesson11_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson11()
+    }
+}
+
+struct Lesson11Second: View {
+    let fruit: String
+    
+    var body: some View {
+        Text(fruit)
+    }
+}
+
+struct Lesson11Second_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson11Second(fruit: "Apple")
+    }
+}
